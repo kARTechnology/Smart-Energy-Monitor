88f00f0e216a9cf49415c369f345929485ff3dd1
Karthikeyan <karthikx10@gmail.com>
2018-02-16 11:01:19 +0530

    Revert "changes to arduino"
    This reverts commit 82c2190f2285c5c069eb514dd268ea8b2e40627c.

--- Arduino/i2c/i2c.ino
+++ Arduino/i2c/i2c.ino
@@ -1,36 +1,34 @@
 #include <Wire.h>
 #include "EmonLib.h"
 #define VOLT_CAL 350
+char my_str[] = "230,1.5,2.3,1^~^";
 //pins setting...
 const int relayPin1 = 5;
 const int relayPin2 = 6;
 const int relayPin3 = 7;
 const int relayPin4 = 8;
 const int motionPin = 9;
-const int currentPin1 = A7;
-const int currentPin2 = A6;
+const int currentPin1 = 19;
+const int currentPin2 = 22;
 const int voltagePin = 23;
 const int endUserSetupPin = 2;
 
 volatile bool endUserSetup = false;
 bool netConnected = false;
-char temp[30]; 
-String str;
 
 EnergyMonitor emon1;
 float voltage, current1, current2;
 int  motion;
 
+
 void setRelayState(int relaynum, int state) {
-  if (state == 1)          digitalWrite(relaynum, LOW);
+  if (state == 1)     digitalWrite(relaynum, LOW);
   else if (state == 0)     digitalWrite(relaynum, HIGH);
 }
-
 void startEndUserSetup() {
   Serial.println("endusersetup initiated by user physically...");
   endUserSetup = true;
 }
-
 void setup()
 {
   //set digital pins
@@ -63,16 +61,19 @@ void setup()
   emon1.voltage(voltagePin, VOLT_CAL, 1.7); //input pin, calibration, phase_shift
 }
 
+
+
 void loop()
 {
-  voltage  =  getVoltage();
-  current1 = getCurrent(currentPin1, 66); 
-  current2 = getCurrent(currentPin2, 66);
-  motion   = getMotionStatus();
+  voltage =  getVoltage();
+  current1 = getCurrent(currentPin1, 66);
+  current2 =  getCurrent(currentPin2, 66);
+  motion = getMotionStatus();
 }
 
 float getCurrent(int sensorIn, int mVperAmp)
 {
+  float result;
   int readValue;             //value read from the sensor
   int maxValue = 0;          // store max value here
   int minValue = 1024;          // store min value here
@@ -83,26 +84,35 @@ float getCurrent(int sensorIn, int mVperAmp)
     readValue = analogRead(sensorIn);
     // see if you have a new maxValue
     if (readValue > maxValue)
-      maxValue = readValue; /*record the maximum sensor value*/
+    {
+      /*record the maximum sensor value*/
+      maxValue = readValue;
+    }
     if (readValue < minValue)
-      minValue = readValue; /*record the minimum sensor value*/
+    {
+      /*record the minimum sensor value*/
+      minValue = readValue;
+    }
   }
-  // Subtract min from max and get peak to peak current
-  float p2p = ((maxValue - minValue) * 5.0) / 1024.0;
-  double  rms = (p2p / 2.0) * 0.707; // 2.5V DC offset For a sine wave, the rms value is 0.707 times (that is root 2) the peak value
-  return (rms * 1000) / mVperAmp;
+
+  // Subtract min from max
+  result = ((maxValue - minValue) * 5.0) / 1024.0;
+
+
+  double  VRMS = (result / 2.0) * 0.707; //root 2 is 0.707
+  return (VRMS * 1000) / mVperAmp;
 }
+
 
 float getVoltage() {
   emon1.calcVI(20, 2000);
   return emon1.Vrms;
 }
-
 int  getMotionStatus() {
-  return  digitalRead(motionPin) == true ? 1 : 0;
+  return  digitalRead(motionPin) ==true? 1:0;
 }
 
-/* i2c events */
+
 void receiveEvent(int howMany) //howMany=num. of bytes received
 {
   int  a, b;
@@ -112,6 +122,7 @@ void receiveEvent(int howMany) //howMany=num. of bytes received
   Serial.print(a);
   Serial.print("=");
   Serial.println(b);
+
   switch (a)
   {
     case 5:   digitalWrite(LED_BUILTIN, b); break;       //set LED_BUILTIN
@@ -122,7 +133,7 @@ void receiveEvent(int howMany) //howMany=num. of bytes received
     case 0:   netConnected = b;                          //set net status
   }
 }
-
+char txt[30]; String str;
 void requestEvent()
 {
   Serial.println(F("Requested..."));
@@ -139,16 +150,20 @@ void requestEvent()
     Wire.write("Waiting for net^~^");
     return;
   }
-  str = String(voltage) + "," + String(current1*voltage) + "," + String(current2*voltage) + "," + String(motion);
-  Serial.println(str);
 
-  // Convert String to char array to send via i2c
-  int i;
+  str = String(voltage) + "," + String(current1) + "," + String(current2) + "," + String(motion);
+
+  Serial.println(F("hola"));
+  int i ;
   for (   i = 0 ; i < str.length() ; i++ ) {
-    temp[i] =   str[i];
+    txt[i] =   str[i];
   }
-  temp[i++] = '^';
-  temp[i++] = '~';
-  temp[i++] = '^';
-  Wire.write(temp);
+  txt[i++] = '^';
+  txt[i++] = '~';
+  txt[i++] = '^';
+
+  Serial.println(F("hola"));
+
+  Wire.write(txt);
+
 }
